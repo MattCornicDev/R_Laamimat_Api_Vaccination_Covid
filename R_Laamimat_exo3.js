f502e7c03c52684542f03663256ef277c7dee904
mattCornicDev <glgharib@gmail.com>
2021-06-21 18:36:08 +0200

    Affichage de la phrase

--- R_Laamimat_exo3.js
+++ R_Laamimat_exo3.js
@@ -7,9 +7,9 @@ var affichage = document .querySelector('#result');
 
 //Déclaration et initialisation des variables utiles 
 var covid; // données au format jason
-var host = "";//hospitalisé
+var dose1 = "";//injection premiere dose 
 var admise ="";//admise
-var gueri ="";//guéri
+var couv_comp ="";//guéri
 var date="";//date
 var dep="";//departement
 var sexe="";
@@ -21,8 +21,7 @@ var age="";
 
     
 
-    let url="https://public.opendatasoft.com/api/records/1.0/search/?dataset=covid-19-france-vaccinations-age-sexe-dep&q=Nord&lang=FR&rows=1&sort=date&facet=couv_dose1&facet=couv_complet&refine.dep_name=Nord";
-
+     url="https://public.opendatasoft.com/api/records/1.0/search/?dataset=covid-19-france-vaccinations-age-sexe-dep&q=Nord&lang=FR&rows=1&sort=date&facet=couv_dose1&facet=couv_complet&refine.dep_name=Nord";
     var req = fetch (url);
     
     req.then (function(response){response.text() .then(function (covid){
@@ -30,16 +29,15 @@ var age="";
            
             var objCovid = JSON.parse(covid);
 
-            date = objCovid.parameters.facet[0].date;
-            admise = objCovid.parameters.facet[0].dep_name;
-            host = objCovid.parameters.facet[0].variable;
-            dep = objCovid.parameters.facet[0].dep_area_code;
+            date = objCovid.records[0].fields.date;
+            dep = objCovid.records[0].fields.dep_name;
+            pop = objCovid.records[0].fields.couv_dose1;
+          //  dose1 = objCovid.records[2].couv_dose1;
+            couv_comp = objCovid.records[0].fields.couv_complet;
+
             console.log(date);
-            
-           
-            affichage.innerHTML = "Résultat : " + date + " dans le " + dep + ", il y a " + host + " personnes hospitalisées, il y a eu " + admise + " nouvelles personnes admises en réanimation et " + gueri + " personnes ont guéri du COVID";
-                
-                 
+                    
+            affichage.innerHTML = date + "dans le " + dep + " , " + pop + "% de la poplulation a reçu une première dose de vaccin et " + couv_comp + "% à une couverture complémentaire" ;     
         });
     
     }).catch(error => alert("Erreur : " + error ));
